8f4d1e20fd23510c7c51255f736af6e9a2899b0d
Walace Filiciano <walace_feliciano@hotmail.com>
2020-07-07 16:27:15 -0300

    Diagonal Negativos

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Negativos_Diagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] mat = new int[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] s = Console.ReadLine().Split(' ');
+                for (int j = 0; j < n; j++)
+                {
+                    mat[i, j] = int.Parse(s[j]);
+                }
+            }
+            Console.WriteLine("Main diagonal: ");
+            for (int i = 0; i < n; i++){
+                Console.Write(mat[i,i] + " " );
+            }
+            Console.WriteLine();
+
+            int cont = 0;
+            for(int i = 0; i < n; i++){
+                for(int j = 0; j < n; j++){
+                    if(mat[i,j] < 0){
+                        cont++;
+                     
+                    }
+
+                }
+
+            }
+            Console.Write("Negative numbers = " + cont);
+        }
+    }
+}
+
